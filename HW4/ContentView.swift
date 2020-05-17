b55c5490ff8bd17a7c9f95cac2b353549dda842b
dejes <jack940197@gmail.com>
2020-05-17 17:37:13 +0800

    Initial Commit

---
+++ HW4/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HW4
+//
+//  Created by User10 on 2020/5/17.
+//  Copyright © 2020 none. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
